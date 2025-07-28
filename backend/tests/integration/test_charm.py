4c3e58cbbe6737db65718f594c609660bd0a99b7
Mateusz Kulewicz <mateusz.kulewicz@canonical.com>
2025-07-28 17:57:02 +0200

    feat: Initial backend charm

---
+++ backend/tests/integration/test_charm.py
@@ -0,0 +1,27 @@
+# Copyright 2025 Canonical Ltd.
+# See LICENSE file for licensing details.
+import logging
+import pytest
+from jubilant import Juju, all_blocked
+from pathlib import Path
+from helpers import BACKEND_SERVER_APP, BACKEND_SERVER_RESOURCES
+
+logger = logging.getLogger(__name__)
+
+
+@pytest.mark.setup
+def test_deploy_litmus_backend_server(juju: Juju, charm: Path):
+    juju.deploy(charm, BACKEND_SERVER_APP, resources=BACKEND_SERVER_RESOURCES, trust=True)
+
+    # backend server will be blocked because of missing control plane integrations
+    juju.wait(
+        lambda status: all_blocked(status, BACKEND_SERVER_APP),
+        timeout=1000,
+        delay=10,
+        successes=5,
+    )
+
+
+@pytest.mark.teardown
+def test_teardown(juju: Juju):
+    juju.remove_application(BACKEND_SERVER_APP)
