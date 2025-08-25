ff439f85355e4ea9d751e600c72369f5343d9076
michael <michael.dmitry98@gmail.com>
2025-08-25 11:25:32 +0300

    feat: add litmus_auth integration

--- backend/tests/unit/test_pebble_plan.py
+++ backend/tests/unit/test_pebble_plan.py
@@ -2,6 +2,7 @@
 # See LICENSE file for licensing details.
 from ops.testing import State
 from dataclasses import replace
+from conftest import db_remote_databag, auth_remote_databag
 
 
 def test_pebble_plan_minimal(ctx, backend_container):
@@ -50,11 +51,7 @@ def test_pebble_plan_with_database_relation(ctx, backend_container, database_rel
     # GIVEN a running container with a database relation
     database_relation = replace(
         database_relation,
-        remote_app_data={
-            "username": "admin",
-            "password": "pass",
-            "uris": "uri.fqdn.1:port,uri.fqdn.2:port",
-        },
+        remote_app_data=db_remote_databag(),
     )
     state = State(containers=[backend_container], relations=[database_relation])
 
@@ -68,5 +65,55 @@ def test_pebble_plan_with_database_relation(ctx, backend_container, database_rel
     ]["environment"]
     assert expected_env_vars.issubset(actual_env_vars.keys())
 
-    # AND the pebble service is running
+    # AND the pebble service is NOT running
+    assert not backend_container_out.services.get("litmuschaos-server").is_running()
+
+
+def test_pebble_plan_with_auth_relation(ctx, backend_container, auth_relation):
+    expected_env_vars = {
+        "LITMUS_AUTH_GRPC_ENDPOINT",
+        "LITMUS_AUTH_GRPC_PORT",
+    }
+    # GIVEN a running container with an auth relation
+    auth_relation = replace(
+        auth_relation,
+        remote_app_data=auth_remote_databag(),
+    )
+    state = State(containers=[backend_container], relations=[auth_relation])
+
+    # WHEN a relation changed event is fired
+    state_out = ctx.run(ctx.on.relation_changed(auth_relation), state=state)
+
+    # THEN litmus backend server pebble plan is generated with extra db env vars
+    backend_container_out = state_out.get_container(backend_container.name)
+    actual_env_vars = backend_container_out.plan.to_dict()["services"][
+        "litmuschaos-server"
+    ]["environment"]
+    assert expected_env_vars.issubset(actual_env_vars.keys())
+
+    # AND the pebble service is NOT running
+    assert not backend_container_out.services.get("litmuschaos-server").is_running()
+
+
+def test_pebble_service_running(
+    ctx, backend_container, auth_relation, database_relation
+):
+    # GIVEN a running container with an auth and a database relation
+    auth_relation = replace(
+        auth_relation,
+        remote_app_data=auth_remote_databag(),
+    )
+    database_relation = replace(
+        database_relation,
+        remote_app_data=db_remote_databag(),
+    )
+    state = State(
+        containers=[backend_container], relations=[auth_relation, database_relation]
+    )
+
+    # WHEN a relation changed event is fired
+    state_out = ctx.run(ctx.on.relation_changed(auth_relation), state=state)
+
+    # THEN litmus backend server pebble service is running
+    backend_container_out = state_out.get_container(backend_container.name)
     assert backend_container_out.services.get("litmuschaos-server").is_running()
