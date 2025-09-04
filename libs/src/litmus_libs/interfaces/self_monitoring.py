5ecb8f9b905907ad42e8ca84b6fb52f7c2fa6353
Pietro Pasotti <pietro.pasotti@canonical.com>
2025-09-04 13:57:31 +0200

    fix lint

--- libs/src/litmus_libs/interfaces/self_monitoring.py
+++ libs/src/litmus_libs/interfaces/self_monitoring.py
@@ -1,3 +1,7 @@
+# Copyright 2025 Canonical Ltd.
+# See LICENSE file for licensing details.
+"""Generic self-monitoring integration wrappers for all litmus charms."""
+
 from typing import Dict, Optional
 
 import ops
