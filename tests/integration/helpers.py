e1c336b903b8c12830d62a943579a9937ab47aaf
Luca Bello <luca.bello@canonical.com>
2026-03-23 16:22:02 +0100

    try exceptions as logging

--- tests/integration/helpers.py
+++ tests/integration/helpers.py
@@ -117,6 +117,7 @@ def deploy_control_plane(
         juju.deploy("traefik-k8s", channel="latest/edge", app=TRAEFIK_APP, trust=True)
         juju.integrate(TRAEFIK_APP, f"{CHAOSCENTER_APP}:ingress")
         apps_to_wait_for.append(TRAEFIK_APP)
+        # raise Exception("We're now at the end of traefik")  # FIXME: Remove this
 
     if with_tls:
         juju.deploy(SELF_SIGNED_CERTIFICATES_APP, app=SELF_SIGNED_CERTIFICATES_APP)
@@ -132,6 +133,7 @@ def deploy_control_plane(
         if with_traefik:
             juju.integrate(f"{TRAEFIK_APP}:certificates", SELF_SIGNED_CERTIFICATES_APP)
         apps_to_wait_for.append(SELF_SIGNED_CERTIFICATES_APP)
+        raise Exception("We're now at the end of with_tls")  # FIXME: Remove this
 
     juju.integrate(f"{AUTH_APP}:database", MONGO_APP)
     juju.integrate(f"{AUTH_APP}:http-api", CHAOSCENTER_APP)
@@ -147,6 +149,8 @@ def deploy_control_plane(
     juju.grant_secret("cc-users", app=CHAOSCENTER_APP)
     juju.config(app=CHAOSCENTER_APP, values={"user_secrets": secret_uri})
 
+    raise Exception("We're now before wait_for_idle")  # FIXME: Remove this
+
     if wait_for_idle:
         logger.info("waiting for the control plane to be active/idle...")
         juju.wait(
@@ -157,6 +161,8 @@ def deploy_control_plane(
             timeout=600,
             delay=30,
         )
+
+    raise Exception("We're now after wait_for_idle")  # FIXME: Remove this
 
 
 def get_login_response(
