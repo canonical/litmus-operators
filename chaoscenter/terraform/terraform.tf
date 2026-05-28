af7d56ef9609408ea246dea6ce1e000afe679af5
Luca Bello <luca.bello@canonical.com>
2026-05-28 09:51:01 +0200

    feat(terraform): Support for Juju provider v2

--- chaoscenter/terraform/terraform.tf
+++ chaoscenter/terraform/terraform.tf
@@ -2,11 +2,10 @@
 # See LICENSE file for licensing details.
 
 terraform {
-  required_version = ">= 1.5"
   required_providers {
     juju = {
       source  = "juju/juju"
-      version = "~> 1.0"
+      version = ">= 1.0"
     }
   }
 }
